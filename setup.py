9b7fe3bc1e6ef9a37cf7525e039d48c9542122f9
Shoaib7171 <shoaibmohammed71@gmail.com>
2024-09-05 18:58:25 +0530

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '1.0.0',
+    author= 'wiz_shoaib',
+    author_email= 'shoaibmohammed71@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
